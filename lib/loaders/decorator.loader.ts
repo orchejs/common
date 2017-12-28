cfe4dcb23d93d10c7c284fd4c5afc3ad4354a862
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-12-28 02:24:20 -0200

    refactor: changing decorator loader for functional paradigm.

--- lib/loaders/decorator.loader.ts
+++ lib/loaders/decorator.loader.ts
@@ -6,79 +6,82 @@
  * found in the LICENSE file at https://github.com/orchejs/common/LICENSE
  */
 import { FileMatcher, FindOptions } from 'file-matcher';
+import * as R from 'ramda';
 import { PathUtils } from '../utils';
 import * as path from 'path';
 
-/**
- * @class
- * @description
- * This class is responsible for loading files after searching for decorators. It uses the
- * file-matcher module, which recursivelly looks for files that contains the informed decorators.
- */
-export class DecoratorLoader {
-  /**
-   * Looks for files containing the decorators as specified in decoratorNames param. After finding
-   * it will load the files to load the annotation infos.
-   * 
-   * @param {string[]} decoratorNames the decorator names that will be searched for.
-   * @param {string} directory the directory to search for decorators. The default value is the
-   * app root path.
-   * @returns a Promise with all the matches and loaded files.
-   */
-  loadDecorators(
-    decoratorNames: string[],
-    directory?: string
-  ): Promise<string[]> {
-    return new Promise(async (resolve, reject) => {
-      if (!decoratorNames || decoratorNames.length === 0) {
-        reject('You must tell me at least one decorator name to look for.');
-        return;
-      }
+const validateDecoratorNamesParameter = (decoratorNames: string[]): boolean => {
+  return decoratorNames && decoratorNames.length > 0;
+};
 
-      const es5Pattern: string = decoratorNames.join('|');
-      const es6Pattern: string = decoratorNames.join('|@');
-      const decoratorsRegex = new RegExp(
-        ['((?=.[^]*__decorate)', '(?=.[^]*', es5Pattern, '))', '|(@', es6Pattern, ')'].join(''),
-        'i'
-      );
-
-      let dirToSearch: string = PathUtils.appRoot;
-      if (directory) {
-        dirToSearch = path.join(dirToSearch, directory);
-      }
-
-      const fileMatcher = new FileMatcher();
-      const criteria: FindOptions = {
-        path: dirToSearch,
-        fileFilter: {
-          fileNamePattern: [
-            '**/*.js',
-            '**/*.ts',
-            '!decorator.loader*',
-            '!node_modules',
-            '!typings'
-          ],
-          content: decoratorsRegex
-        },
-        recursiveSearch: true
-      };
-
-      try {
-        const files: string[] = await fileMatcher.find(criteria);
-
-        if (!files || files.length === 0) {
-          reject('No files found!');
-        }
-
-        for (let idx = 0; idx < files.length; idx += 1) {
-          const file = files[idx];
-          require(file);
-        }
-
-        resolve(files);
-      } catch (error) {
-        reject(error);
-      }
-    });
+const validateDirectoryToSearch = (directory: string) => {
+  let dirToSearch: string = PathUtils.appRoot;
+  if (directory) {
+    dirToSearch = path.join(dirToSearch, directory);
   }
-}
+  return dirToSearch;
+};
+
+const validateDecoratorCrawlerEntry = (
+  decoratorNames: string[],
+  directory?: string
+): Promise<any> => {
+  if (!validateDecoratorNamesParameter(decoratorNames)) {
+    return Promise.reject('You must tell me at least one decorator name to look for');
+  }
+  const dirToSearch = validateDirectoryToSearch(directory);
+  return Promise.resolve([decoratorNames, dirToSearch]);
+};
+
+const decoratorCrawler = (decoratorNames: string[], directory: string): Promise<string[]> => {
+  const es5Pattern: string = decoratorNames.join('|');
+  const es6Pattern: string = decoratorNames.join('|@');
+
+  const decoratorsRegex = new RegExp(
+    ['((?=.[^]*__decorate)', '(?=.[^]*', es5Pattern, '))', '|(@', es6Pattern, ')'].join(''),
+    'i'
+  );
+
+  const fileMatcher = new FileMatcher();
+  return fileMatcher.find({
+    path: directory,
+    fileFilter: {
+      fileNamePattern: ['**/*.js', '**/*.ts', '!decorator.loader*', '!node_modules', '!typings'],
+      content: decoratorsRegex
+    },
+    recursiveSearch: true
+  });
+};
+
+const validateDecoratorFiles = (files: string[]): Promise<string[]> => {
+  if (!files || files.length === 0) {
+    return Promise.reject('No files found!');
+  }
+  return Promise.resolve(files);
+};
+
+const requireDecoratorFiles = (files: string[]): Promise<string[]> => {
+  const loadedFiles: string[] = files.map(file => {
+    try {
+      require(file);
+      return file;
+    } catch (error) {}
+  });
+  return Promise.resolve(loadedFiles);
+};
+
+/**
+ * Looks for files containing the decorators as specified in decoratorNames param. After finding
+ * it will load the files to load the annotation infos.
+ *
+ * @param {string[]} decoratorNames the decorator names that will be searched for.
+ * @param {string} directory the directory to search for decorators. The default value is the
+ * app root path.
+ * @returns a Promise with all the matches and loaded files.
+ */
+export const loadDecorators = R.compose(
+  requireDecoratorFiles,
+  validateDecoratorFiles,
+  decoratorCrawler,
+  validateDecoratorCrawlerEntry
+);
