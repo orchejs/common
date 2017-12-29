86e5d96e7164df81200348ec7f210ae779f403da
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-12-29 00:36:48 -0200

    refactor(loaders): finished decorator loader refactor to FP.

--- lib/loaders/decorator.loader.ts
+++ lib/loaders/decorator.loader.ts
@@ -10,11 +10,22 @@ import * as R from 'ramda';
 import { PathUtils } from '../utils';
 import * as path from 'path';
 
-const validateDecoratorNamesParameter = (decoratorNames: string[]): boolean => {
-  return decoratorNames && decoratorNames.length > 0;
+const transformDecoratorNamesToRegex = (decoratorNames: string[]): RegExp => {
+  if (!decoratorNames || decoratorNames.length === 0) {
+    throw new Error('You must tell at least one decorator name to look for');
+  }
+
+  const es5Pattern: string = decoratorNames.join('|');
+  const es6Pattern: string = decoratorNames.join('|@');
+
+  const decoratorPattern = new RegExp(
+    ['((?=.[^]*__decorate)', '(?=.[^]*', es5Pattern, '))', '|(@', es6Pattern, ')'].join(''),
+    'i'
+  );
+  return decoratorPattern;
 };
 
-const validateDirectoryToSearch = (directory: string) => {
+const prepareDirectoryToSearch = (directory: string): string => {
   let dirToSearch: string = PathUtils.appRoot;
   if (directory) {
     dirToSearch = path.join(dirToSearch, directory);
@@ -22,66 +33,64 @@ const validateDirectoryToSearch = (directory: string) => {
   return dirToSearch;
 };
 
-const validateDecoratorCrawlerEntry = (
-  decoratorNames: string[],
-  directory?: string
-): Promise<any> => {
-  if (!validateDecoratorNamesParameter(decoratorNames)) {
-    return Promise.reject('You must tell me at least one decorator name to look for');
-  }
-  const dirToSearch = validateDirectoryToSearch(directory);
-  return Promise.resolve([decoratorNames, dirToSearch]);
-};
-
 const decoratorCrawler = (decoratorNames: string[], directory: string): Promise<string[]> => {
-  const es5Pattern: string = decoratorNames.join('|');
-  const es6Pattern: string = decoratorNames.join('|@');
-
-  const decoratorsRegex = new RegExp(
-    ['((?=.[^]*__decorate)', '(?=.[^]*', es5Pattern, '))', '|(@', es6Pattern, ')'].join(''),
-    'i'
-  );
+  let decoratorPattern;
+  let dirToSearch;
+  try {
+    decoratorPattern = transformDecoratorNamesToRegex(decoratorNames);
+    dirToSearch = prepareDirectoryToSearch(directory);
+  } catch (err) {
+    return Promise.reject(err);
+  }
 
   const fileMatcher = new FileMatcher();
   return fileMatcher.find({
-    path: directory,
+    path: dirToSearch,
     fileFilter: {
       fileNamePattern: ['**/*.js', '**/*.ts', '!decorator.loader*', '!node_modules', '!typings'],
-      content: decoratorsRegex
+      content: decoratorPattern
     },
     recursiveSearch: true
   });
 };
 
-const validateDecoratorFiles = (files: string[]): Promise<string[]> => {
+const validateDecoratorFiles = (files: string[]): string[] => {
   if (!files || files.length === 0) {
-    return Promise.reject('No files found!');
+    throw new Error('No files found!');
   }
-  return Promise.resolve(files);
+  return files;
 };
 
-const requireDecoratorFiles = (files: string[]): Promise<string[]> => {
+const requireDecoratorFiles = (files: string[]) => {
   const loadedFiles: string[] = files.map(file => {
     try {
       require(file);
       return file;
     } catch (error) {}
   });
-  return Promise.resolve(loadedFiles);
+  return loadedFiles;
 };
 
+const dealDecoratorSearch = R.compose(requireDecoratorFiles, validateDecoratorFiles);
+
 /**
- * Looks for files containing the decorators as specified in decoratorNames param. After finding
- * it will load the files to load the annotation infos.
+ * Looks for files containing the decorators as specified in decoratorNames param.
+ * After finding it will load the files to load the annotation infos.
  *
  * @param {string[]} decoratorNames the decorator names that will be searched for.
  * @param {string} directory the directory to search for decorators. The default value is the
  * app root path.
  * @returns a Promise with all the matches and loaded files.
  */
-export const loadDecorators = R.compose(
-  requireDecoratorFiles,
-  validateDecoratorFiles,
-  decoratorCrawler,
-  validateDecoratorCrawlerEntry
-);
+const loadDecorators = (decoratorNames: string[], directory?: string): Promise<string[]> => {
+  return new Promise((resolve, reject) => {
+    decoratorCrawler(decoratorNames, directory)
+      .then(files => {
+        const loadedFiles = dealDecoratorSearch(files);
+        resolve(loadedFiles);
+      })
+      .catch(error => reject(error));
+  });
+};
+
+export { loadDecorators, decoratorCrawler };
